8e8f71ef554a52b45d54e9eda796bbff3b7f9565
simplify <iteria.info@gmail.com>
2018-10-31 18:44:26 +0200

    create a half page forum

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,4 +1,25 @@
 $(function ($) {
+
+// Acc
+$(document).on("click", ".naccs .menu div", function() {
+	var numberIndex = $(this).index();
+
+	if (!$(this).is("active")) {
+		$(".naccs .menu div").removeClass("active");
+		$(".naccs ul li").removeClass("active");
+
+		$(this).addClass("active");
+		$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+		var listItemHeight = $(".naccs ul")
+			.find("li:eq(" + numberIndex + ")")
+			.innerHeight();
+		$(".naccs ul").height(listItemHeight + "px");
+	}
+});
+
+
+
     $(window).on('resize', function(){
     if($(window).width() > 992){
         $('.bl-post:first-child').removeClass('col-lg-4').addClass('col-8');
